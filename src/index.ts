3df5cdfe41971a24cd8a5491eb67feae0534fd11
Katsuma Narisawa <face.the.music721@gmail.com>
2020-04-04 23:53:40 +0900

    Implement sample

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,22 @@
-export const helloWorld = (req: any, res: any) => {
-  res.send("Hello, World");
+import sharp from "sharp";
+import request from "request-promise";
+const SAMPLE_IMAGE_URL =
+  "https://katsumanarisawa.me/_nuxt/img/sunset.768f32b.jpg";
+
+const SAMPLE_CREDIT_URL =
+  "https://katsumanarisawa.me/_nuxt/img/symbol_2_wh.2d8cacc.png";
+
+export const convertImage = async (req: any, res: any) => {
+  const sampleImage = await request.get(SAMPLE_IMAGE_URL, { encoding: null });
+  const sampleCredit = await request.get(SAMPLE_CREDIT_URL, { encoding: null });
+  const image = sharp(Buffer.from(sampleImage))
+    .resize(840, 360)
+    .composite([
+      {
+        input: Buffer.from(sampleCredit),
+      },
+    ]);
+
+  res.type("jpg");
+  res.send(await image.toBuffer());
 };
