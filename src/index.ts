66e77d67dff6e0e175cc69c74860683296ae0a48
Katsuma Narisawa <face.the.music721@gmail.com>
2020-04-05 16:10:04 +0900

    Sample implementation for serverless image processing (#2)
    * Implement sample
    
    * Add logs
    
    * This project is in progress
    
    * Ooops

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,28 @@
-export const helloWorld = (req: any, res: any) => {
-  res.send("Hello, World");
+import sharp from "sharp";
+import request from "request-promise";
+const SAMPLE_IMAGE_URL =
+  "https://katsumanarisawa.me/_nuxt/img/sunset.768f32b.jpg";
+
+const SAMPLE_CREDIT_URL =
+  "https://katsumanarisawa.me/_nuxt/img/symbol_2_wh.2d8cacc.png";
+
+export const convertImage = async (req: any, res: any) => {
+  console.log("Start convertImage");
+  const sampleImage = await request.get(SAMPLE_IMAGE_URL, { encoding: null });
+  console.log("Finished fetching sampleImage");
+
+  const sampleCredit = await request.get(SAMPLE_CREDIT_URL, { encoding: null });
+  console.log("Finished fetching sampleCredit");
+
+  const image = sharp(Buffer.from(sampleImage))
+    .resize(840, 360)
+    .composite([
+      {
+        input: Buffer.from(sampleCredit),
+      },
+    ]);
+  console.log("Finished resizing");
+
+  res.type("jpg");
+  res.send(await image.toBuffer());
 };
